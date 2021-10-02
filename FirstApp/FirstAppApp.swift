ae2d41e524dec488f886e4189bcf2f6b583e943e
JeanPaulll <jeanpaulwebb@gmail.com>
2021-10-01 23:41:49 -0300

    add FirstAppt

---
+++ FirstApp/FirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppApp.swift
+//  FirstApp
+//
+//  Created by Jean Paul on 01/10/21.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
